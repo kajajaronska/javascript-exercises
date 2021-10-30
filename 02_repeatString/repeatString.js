10378d5c40ebda1f1daf99bb9c44f116cb2274fe
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-10-30 20:29:01 +0100

    Solution to exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // if (num > 0) {
+    // return string.repeat(num)
+    // } else {return "ERROR"};
 
+    let newString = '';
+
+    if(num > 0) {
+        for(let i=1; i < num + 1; i++) {
+            newString = newString + string;
+        }
+    } else if(num === 0) {
+        return newString;
+    } else {
+        return "ERROR"
+    }
+        return newString;
 };
 
 // Do not edit below this line
