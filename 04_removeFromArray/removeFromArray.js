01ff1f42635a463ce66a6187f4d13ea37b5723a2
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-10-30 22:27:34 +0100

    Solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removed) {
+    let removedArr = removed;
+
+    for (let i=0; i <= removedArr.length-1; i++) {
+        let index = arr.indexOf(removedArr[i])
+        if (index > -1) arr.splice(index, 1);
+    }
+
+    return arr;
 
 };
 
