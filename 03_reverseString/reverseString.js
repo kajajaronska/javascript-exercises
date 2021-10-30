9a363bb517c665200d8f71e74270469e6463c00b
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-10-30 21:39:40 +0100

    Solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
 const reverseString = function(str) {
+    let letters = [];
+
+    for (let i = str.length-1; i >= 0; i--) {
+        letters.push(str[i]);
+    }
+
+    return letters.join('');
 
 };
 
