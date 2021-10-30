3950d5fa793ccb0bbe5475cff463d339c2a6afe3
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-10-30 22:44:28 +0100

    Solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if (typeof start != 'number' || typeof end != 'number' || start < 0 || end < 0 ) {
+        return "ERROR"
+    } else if (start < end) {
+        for (let i=start; i <= end; i++) {
+            sum += i
+        }
+    } else {
+        for (let i=start; i >= end; i--) {
+            sum += i
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
