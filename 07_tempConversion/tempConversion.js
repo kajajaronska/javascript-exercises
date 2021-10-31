8151b0c62ad9405339b2d8aaab462a7777eaf171
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-10-31 13:31:39 +0000

    Solution to exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
+const ftoc = function(num) {
+  let cTemp = (num - 32) * (5/9);
+
+  if (cTemp % 1 != 0) {
+    return Number(cTemp.toFixed(1));
+  } else {
+    return Number(cTemp.toFixed(0));
+  }
 
 };
 
-const ctof = function() {
+const ctof = function(num) {
+  let fTemp = num * (9/5) + 32;
 
+  if (fTemp % 1 != 0) {
+    return Number(fTemp.toFixed(1));
+   } else {
+     return Number(fTemp.toFixed(0));
+   }
 };
 
 // Do not edit below this line
