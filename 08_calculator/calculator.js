fb8e2165ea2e2097d0250aae885cd78041bbba43
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-11-12 17:43:31 +0000

    Solution to exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  if(a == 0 && b == 0) return 0;
+
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, curr) => {
+    return acc += curr},0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((acc,curr) => {return acc *= curr},
+  )};
 
+const power = function(a,b) {
+	return parseInt(a) ** parseInt(b);
 };
 
-const power = function() {
-	
+
+
+const factorial = function(num) {
+  let arr = [];
+  for(let i = num; i >= 1; i--) {
+    arr.unshift(i);
+  };
+
+  if(num === 0 || num === 1) return 1;
+
+  return arr.reduce((acc,curr)=>{ return acc *= curr},1);
 };
 
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
