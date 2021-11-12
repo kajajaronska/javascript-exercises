5c838d10ee33f5f816fcd241adc5b5ef5cf786ab
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-11-12 23:29:33 +0000

    Solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
+    let arr = str
+        .split('')
+        .filter((letter)=> {
+        return punctuation.indexOf(letter) === -1;});
+
+    let cleanedStr = arr.join('').toLowerCase().replace(/\s/g, '');
+    let reversedStr = arr.reverse().join('').toLowerCase().replace(/\s/g, '');
+    let isPalindrome;
+
+    (reversedStr === cleanedStr) ? isPalindrome = true : isPalindrome = false;
+
+    console.log(cleanedStr, reversedStr);
+    return isPalindrome;
 };
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
