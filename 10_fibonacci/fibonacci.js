6d6308026e3f5853232ea125cfb501c88710f842
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-11-13 10:37:30 +0000

    Solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // Fibonacci sequence rule
+    // x[num] = x[num-1] + x[num-2];
+    
+    let y = +num;
+    let x = [0,1];
 
+    if(y > 0) {
+        for(let i=2; i <= 317811; i++){
+            x.push(x[i-1]+x[i-2]);
+        };
+        
+        return x[num];
+    } else {
+        return z = "OOPS";
+    };
 };
 
 // Do not edit below this line
